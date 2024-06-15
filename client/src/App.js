b981b3e895b2d4ed862f35c16d4f048c9047c824
Siddharth Ruria <siddharthruria10@gmail.com>
2024-06-15 09:12:32 +0530

    initial-setup

---
+++ client/src/App.js
@@ -0,0 +1,80 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          My Blog
+        </a>
+        <nav>
+          <a href="">
+            Login
+          </a>
+          <a href="">
+            Register
+          </a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+        <img
+          src="https://ichef.bbci.co.uk/news/1024/cpsprodpb/1239/live/e2168ce0-2853-11ef-a07f-09e6d1fd4403.jpg.webp"
+          alt=""
+        />
+        </div>
+        <div className="texts">
+          <h2>
+            Elon Musk unexpectedly drops lawsuit against OpenAI and Sam Altman
+          </h2>
+          <p>
+            Elon Musk has unexpectedly asked a California court to withdraw a
+            legal case against OpenAI and its boss Sam Altman, which accused
+            them of abandoning the firm's founding mission of developing
+            artificial intelligence (AI) for humanity's benefit.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image">
+        <img
+          src="https://ichef.bbci.co.uk/news/1024/cpsprodpb/1239/live/e2168ce0-2853-11ef-a07f-09e6d1fd4403.jpg.webp"
+          alt=""
+        />
+        </div>
+        <div className="texts">
+          <h2>
+            Elon Musk unexpectedly drops lawsuit against OpenAI and Sam Altman
+          </h2>
+          <p>
+            Elon Musk has unexpectedly asked a California court to withdraw a
+            legal case against OpenAI and its boss Sam Altman, which accused
+            them of abandoning the firm's founding mission of developing
+            artificial intelligence (AI) for humanity's benefit.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+      <div className="image">
+        <img
+          src="https://ichef.bbci.co.uk/news/1024/cpsprodpb/1239/live/e2168ce0-2853-11ef-a07f-09e6d1fd4403.jpg.webp"
+          alt=""
+        />
+        </div>
+        <div className="texts">
+          <h2>
+            Elon Musk unexpectedly drops lawsuit against OpenAI and Sam Altman
+          </h2>
+          <p>
+            Elon Musk has unexpectedly asked a California court to withdraw a
+            legal case against OpenAI and its boss Sam Altman, which accused
+            them of abandoning the firm's founding mission of developing
+            artificial intelligence (AI) for humanity's benefit.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
